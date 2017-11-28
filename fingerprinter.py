4667715422ff4c177e088a1353fcbe57350dabc1
Ope Aina <barmmie@gmail.com>
2017-11-28 08:37:25 +0000

    quick demo

---
+++ fingerprinter.py
@@ -0,0 +1,22 @@
+import warnings
+import json
+warnings.filterwarnings("ignore")
+
+from dejavu import Dejavu
+from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
+
+# load config from a JSON file (or anything outputting a python dictionary)
+with open("dejavu.cnf.SAMPLE") as f:
+    config = json.load(f)
+
+if __name__ == '__main__':
+
+	# create a Dejavu instance
+	djv = Dejavu(config)
+
+	# Fingerprint all the mp3's in the directory we give it
+	djv.fingerprint_directory("ads", [".mp4"])
+
+	# Recognize audio from a file
+	song = djv.recognize(FileRecognizer, "ads_mashup.mp4")
+	print "From file we recognized: %s\n" % song
